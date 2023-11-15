78701fe1ff7e0d43ab021bae119298568fe89411
João Carrijo <joao.carrijo@luby.com.br>
2023-11-15 11:22:04 -0300

    Implementação de testes de carga

---
+++ ChargeTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using NBomber.CSharp;
+
+using var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("First Charge Test", async context =>
+{
+    var response = await httpClient.GetAsync("https://localhost:7180/api/Product");
+    
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+.WithoutWarmUp()
+.WithLoadSimulations(
+    Simulation.Inject(rate: 10,
+        interval: TimeSpan.FromSeconds(1),
+        during: TimeSpan.FromSeconds(30))
+);
+
+NBomberRunner.RegisterScenarios(scenario)
+    .Run();
